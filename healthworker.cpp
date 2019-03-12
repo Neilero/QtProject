82a92638c01cb96ec0a446efc989f8c738f44d2f
Amane <lucie.vallaude19@gmail.com>
2019-03-12 01:26:30 +0100

    healthworker.cpp : Add get & set + builders

--- healthworker.cpp
+++ healthworker.cpp
@@ -1,6 +1,17 @@
 #include "healthworker.h"
 
-HealthWorker::HealthWorker()
+HealthWorker::HealthWorker(QString name, QString firstname, HealthWorkerType type = computerScientist, QString login, QString password)
 {
+    this->setName(name);
+    this->setFirstname(firstname);
+    this->setType(type);
+    this->setLogin(login);
+    this->setPassword(password);
+}
 
+HealthWorker::HealthWorker(QString name, QString firstname, HealthWorkerType type)
+{
+    this->setName(name);
+    this->setFirstname(firstname);
+    this->setType(type);
 }
